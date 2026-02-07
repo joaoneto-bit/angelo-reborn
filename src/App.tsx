7e2129ae491996e477d298d0a0168d056d669fac
joaoneto-bit <joao.neto@fyld.pt>
2026-02-07 15:30:38 +0000

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/angelo-reborn">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/produtos" element={<ProductsPage />} />
